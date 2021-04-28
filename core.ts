8972f311a9abcea8fb86edd5f8f7bccafe833e94
Simon Le-Clerc <slc.le.clerc@gmail.com>
2021-04-28 23:05:29 +0200

    green: handle strike rolls

--- core.ts
+++ core.ts
@@ -11,11 +11,18 @@ export class Game {
     let score = 0;
     let firstInFrame = 0;
     for (let frame = 0; frame < 10; frame++) {
-      if (this.isSpare(firstInFrame)) {
+      //strike
+      if (this._rolls[firstInFrame] == 10) {
+        score +=
+          10 + this._rolls[firstInFrame + 1] + this._rolls[firstInFrame + 2];
+        firstInFrame++;
+      } else if (this.isSpare(firstInFrame)) {
         score += 10 + this._rolls[firstInFrame + 2] ?? 0;
         firstInFrame += 2;
       } else {
-        score += (this._rolls[firstInFrame] ?? 0) + (this._rolls[firstInFrame + 1] ?? 0);
+        score +=
+          (this._rolls[firstInFrame] ?? 0) +
+          (this._rolls[firstInFrame + 1] ?? 0);
         firstInFrame += 2;
       }
     }
@@ -23,6 +30,9 @@ export class Game {
   }
 
   private isSpare(firstInFrame: number) {
-    return (this._rolls[firstInFrame] ?? 0) + (this._rolls[firstInFrame + 1] ?? 0) == 10;
+    return (
+      (this._rolls[firstInFrame] ?? 0) + (this._rolls[firstInFrame + 1] ?? 0) ==
+      10
+    );
   }
 }
