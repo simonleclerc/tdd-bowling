6acbaa00b4b533d85a577a92eec4fb22a1a40a18
Simon Le-Clerc <slc.le.clerc@gmail.com>
2021-04-28 22:25:04 +0200

    red: roll only in the gutter

--- core.test.ts
+++ core.test.ts
@@ -10,4 +10,12 @@ describe("Bowling Game", () => {
   it("can roll", () => {
     g.roll(0);
   });
+
+  it("rolls only in the gutter", () => {
+    for (let i = 0; i < 20; i++) {
+      g.roll(0);
+    }
+
+    expect(g.score()).toEqual(0);
+  });
 });
