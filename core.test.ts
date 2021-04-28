a0973fc06c19f157ac49c1c37ab7e74a9faba524
Simon Le-Clerc <slc.le.clerc@gmail.com>
2021-04-28 22:22:49 +0200

    refactor: game variable and delete create game test

--- core.test.ts
+++ core.test.ts
@@ -1,12 +1,13 @@
 import { Game } from "./core";
 
 describe("Bowling Game", () => {
-  it("can create game", () => {
-    const g = new Game();
+  let g: Game;
+
+  beforeEach(() => {
+    g = new Game();
   });
 
   it("can roll", () => {
-    const g = new Game();
     g.roll(0);
   });
 });
