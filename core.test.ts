cab6fb5a3e3ff36f784aca513ba5c328644a14f4
Simon Le-Clerc <slc.le.clerc@gmail.com>
2021-04-28 22:16:54 +0200

    red: can roll

--- core.test.ts
+++ core.test.ts
@@ -1,7 +1,12 @@
-import { Game } from './core';
+import { Game } from "./core";
 
 describe("Bowling Game", () => {
   it("can create game", () => {
     const g = new Game();
+  });
+
+  it("can roll", () => {
+    const g = new Game();
+    g.roll(0);
   });
 });
